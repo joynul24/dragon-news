ff4c357eabe1ff150eb765d52c26c16ef2fd8ffd
joynul24 <joynula919@gmail.com>
2026-02-13 23:58:27 +0600

    Added stucture components and make Header component responsive with Tailwind

---
+++ src/components/shared/Header.jsx
@@ -0,0 +1,17 @@
+import moment from "moment"
+import logo from "../../assets/logo.png"
+
+export default function Header() {
+    return (
+        <div className="flex flex-col justify-center items-center py-5 md:py-10 px-2">
+            {/* Responsive image */}
+            <img className="w-40 sm:w-60 md:w-80 lg:w-121.25 h-auto" src={logo} alt="logo" />
+            {/* Tagline */}
+            <h4 className="text-gray-600 mt-5 mb-2 text-center text-sm sm:text-base md:text-lg"> Journalism Without Fear or Favour</h4>
+            {/* Date with moment.js */}
+            <p className="text-gray-500 text-xs sm:text-sm md:text-base">
+                {moment().format("dddd, MMMM D, YYYY")}
+            </p>
+        </div>
+    )
+}
---
+++ src/layouts/HomeLayout.jsx
@@ -0,0 +1,13 @@
+import Header from "../components/shared/Header";
+
+export default function HomeLayout() {
+  return (
+    <div className="container mx-auto font-poppins">
+      <header>
+        <Header></Header>
+      </header>
+      <nav></nav>
+      <main></main>
+    </div>
+  )
+}
